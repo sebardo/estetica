6716b3ed56bc7a5405a617019ebca8bd14ce45c1
Sebardo <sebastian.sasturain@gmail.com>
2017-12-21 13:01:37 +0100

    Add rebuild file bis 2

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod -R 777 app/cache/ app/logs/ 
-php app/console doctrine:schema:drop --force && php app/console doctrine:schema:create && php app/console doctrine:fixtures:load --append
+chmod -R 777 app/cache/ app/logs/ &&
+/opt/cpanel/ea-php56/root/usr/bin/php app/console doctrine:schema:drop --force && /opt/cpanel/ea-php56/root/usr/bin/php app/console doctrine:schema:create && /opt/cpanel/ea-php56/root/usr/bin/php app/console doctrine:fixtures:load --append
 rm -rf app/cache/* app/logs/*
 
